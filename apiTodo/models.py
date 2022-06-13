6f24fd040a817d77db9c34387f5252bd0f0b4d3d
m_emin <mehmeteminbul@gmail.com>
2022-06-13 20:39:16 +0300

    model created

--- apiTodo/models.py
+++ apiTodo/models.py
@@ -1,3 +1,24 @@
+from asyncio import tasks
+from pydoc import describe
+from queue import PriorityQueue
+from turtle import done
 from django.db import models
+from django.forms import BooleanField
 
 # Create your models here.
+
+class Todo(models.Model):
+    task = models.CharField(max_length=50)
+    description = models.TextField()
+    TITLE = (
+        ("H", "High"),
+        ("M", "Medium"),
+        ("L", "Low"),
+    )
+    priority = models.CharField(max_length=50,choices=TITLE,default="L")
+    done = models.BooleanField()
+    updateDate = models.DateTimeField(auto_now=True)
+    createdDate= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task
