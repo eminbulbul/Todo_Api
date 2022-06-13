73e446f1198750c75d9c9f381a3722cb97cea266
m_emin <mehmeteminbul@gmail.com>
2022-06-13 21:46:32 +0300

    views created

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, hello_world, todoList,todoCreate
 
 
 urlpatterns = [
     path('', home),
-    
+    path('hello/', hello_world),
+    path('todolist/', todoList),
+    path('todocreate/', todoCreate),
 ]
--- apiTodo/views.py
+++ apiTodo/views.py
@@ -1,9 +1,33 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Todo
+from .serializers import TodoSerializers
+
 
 # Create your views here.
 def home(request):
     return HttpResponse(
         '<center><h1 style="background-color:powderblue;">Welcome to ApiTodo</h1></center>'
     )
+
+@api_view()
+def hello_world(request):
+    return Response({"message": "Hello, world!"})
+
+@api_view(['GET'])
+def todoList(request):
+    queryset = Todo.objects.all()
+    serializer = TodoSerializers(queryset, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def todoCreate(request):
+    serializer = TodoSerializers(request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
